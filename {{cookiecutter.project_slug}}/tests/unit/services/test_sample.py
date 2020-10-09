498be0c3a822a1e5e66964c04418af56fce1ee8d
Bobson Lin <bobson801104@gmail.com>
2020-10-09 11:08:59 +0800

    Add Dockerfile

--- {{cookiecutter.project_slug}}/tests/unit/services/test_sample.py
+++ {{cookiecutter.project_slug}}/tests/unit/services/test_sample.py
@@ -3,5 +3,6 @@ from tests.unit.services import FakeRepository
 
 def test_service_sample():
     repo = FakeRepository()
+    assert type(repo) == FakeRepository
     # result = list_samples(repo)
     # assert type(result) == dict
